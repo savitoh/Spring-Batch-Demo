2034ebf293a11a1a3212079c78eca342a63fb5d5
savitoh <savioraires10@gmail.com>
2019-03-25 18:28:33 -0300

    Commit inicial dos fontes

---
+++ src/main/java/com/br/savit0h/batchservicedemo/BatchServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.br.savit0h.batchservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchServiceDemoApplication.class, args);
+	}
+
+}
